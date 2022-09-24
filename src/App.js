e89138c6d92e7494de7955b91895704438035af3
mattp15 <matthewrphillips15@gmail.com>
2022-09-23 20:36:03 -0400

    testing user grabbing

--- src/App.js
+++ src/App.js
@@ -6,32 +6,38 @@ function App() {
   return (
     <div>
       <CaravansContainer />
-      {/* <PlayerContainer />
-
-      
-        <input type={'submit'} value='create cards' onClick={()=>{
+      {/* <PlayerContainer /> */}
+      {/* 
+      <input
+        type={'submit'}
+        value='create cards'
+        onClick={() => {
           fetch('http://localhost:3000/cards/createSeed', {
             method: 'POST',
-            body: JSON.stringify({test:'test'}),
-            headers: { 'Content-Type': 'application/json'}
+            body: JSON.stringify({ test: 'test' }),
+            headers: { 'Content-Type': 'application/json' },
           });
-        }} />
-        
-        <input type={'submit'} value='test NewUser' onClick={()=>{
+        }}
+      />
+
+      <input
+        type={'submit'}
+        value='test NewUser'
+        onClick={() => {
           fetch('http://localhost:3000/user/register', {
             method: 'POST',
             body: JSON.stringify({
-              name: 'Dave',
-              gamertag: 'gtag',
-              email: 'me@here.net',
-              password: 'pass',
+              name: 'Matt',
+              gamertag: 'Exitbot',
+              email: 'mattp@gmail.com',
+              password: 'pass1234',
             }),
-            headers: { 'Content-Type': 'application/json'}
+            headers: { 'Content-Type': 'application/json' },
           });
-        }} /> */}
-      
+        }}
+      /> */}
     </div>
-  )
+  );
 }
 
 export default App;
