6317bed37dd6c50a466f99d57a110ef7b8094a84
mattp15 <matthewrphillips15@gmail.com>
2022-09-24 16:48:08 -0400

    got bottom displaying backs on select as well

--- src/Components/PlayField/Player/PlayerDeckBuilding.js
+++ src/Components/PlayField/Player/PlayerDeckBuilding.js
@@ -19,7 +19,7 @@ const PlayerDeckBuilding = (props) => {
       )}
       <ul className='deckBuild-bottomRow'>
         {props.showDeck.map((card, index) => {
-          const imgSrc = 'http://localhost:3000/images/' + card.face;
+          const imgSrc = !props.deck.includes(card) ? 'http://localhost:3000/images/' + card.face : 'http://localhost:3000/images/' + card.back + '.png';
           return (
             <li
               key={'bottom' + index}
