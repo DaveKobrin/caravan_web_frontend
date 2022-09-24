32638bfb040728cf4315e5f55cd82c2f7e42442a
mattp15 <matthewrphillips15@gmail.com>
2022-09-23 20:43:07 -0400

    cathing up, getoneByid last added

--- src/App.js
+++ src/App.js
@@ -37,28 +37,35 @@ function App() {
 <<<<<<< HEAD
         }}
       /> */}
-=======
-        }} /> */}
-        <input type={'submit'} value='test Get Tops Set' onClick={()=>{
+
+      <input
+        type={'submit'}
+        value='test Get Tops Set'
+        onClick={() => {
           fetch('http://localhost:3000/cards/getManyByPattern/set/Tops', {
             method: 'GET',
             headers: { 'Content-Type': 'application/json' },
-          }).then((response) => response.json())
-          .then((data) => {
-            console.log(data);
-          });
-        }} />
-        <input type={'submit'} value='test GetOneById' onClick={()=>{
+          })
+            .then((response) => response.json())
+            .then((data) => {
+              console.log(data);
+            });
+        }}
+      />
+      <input
+        type={'submit'}
+        value='test GetOneById'
+        onClick={() => {
           fetch('http://localhost:3000/cards/getOneById/632d0fa1705f594726aa8136', {
             method: 'GET',
             headers: { 'Content-Type': 'application/json' },
-          }).then((response) => response.json())
-          .then((data) => {
-            console.log(data);
-          });
-        }} />
-
->>>>>>> f3ab35294a663cecdee752f5e7cda3b4f3ad402a
+          })
+            .then((response) => response.json())
+            .then((data) => {
+              console.log(data);
+            });
+        }}
+      />
     </div>
   );
 }
