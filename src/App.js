8382fd7908f413835c39536e262c460c20044797
dkobrin <shdwkat999@yahoo.com>
2022-09-25 17:14:50 -0400

    modified test code in app.js

--- src/App.js
+++ src/App.js
@@ -19,11 +19,6 @@ function App() {
           });
 
         }} />*/}
-        
-        {/* <input type={'submit'} value='test NewUser' onClick={()=>{
-
-        }}
-      />
 
       <input
         type={'submit'}
@@ -33,33 +28,19 @@ function App() {
           fetch('http://localhost:3000/user/register', {
             method: 'POST',
             body: JSON.stringify({
-              name: 'Matt',
-              gamertag: 'Exitbot',
-              email: 'mattp@gmail.com',
-              password: 'pass1234',
+              name: 'Dave',
+              gamertag: 'gtag',
+              email: 'me@here.com',
+              password: 'pass',
             }),
             headers: { 'Content-Type': 'application/json' },
+          }).then((response) => response.json())
+          .then((data) => {
+            console.log(data);
           });
 
         }} /> 
-        <input type={'submit'} value='test Get Tops Set' onClick={()=>{
-          fetch('http://localhost:3000/cards/getManyByPattern/set/Tops', {
-            method: 'GET',
-            headers: { 'Content-Type': 'application/json' },
-          }).then((response) => response.json())
-          .then((data) => {
-            console.log(data);
-          });
-        }} /> */}
-        {/* <input type={'submit'} value='test GetOneById' onClick={()=>{
-          fetch('http://localhost:3000/cards/getOneById/632d0fa1705f594726aa8136', {
-            method: 'GET',
-            headers: { 'Content-Type': 'application/json' },
-          }).then((response) => response.json())
-          .then((data) => {
-            console.log(data);
-          });
-        }} /> */}
+      
 
         <input type={'submit'} value='test login' onClick={()=>{
           fetch('http://localhost:3000/user/login', {
@@ -85,8 +66,18 @@ function App() {
           });
         }} />
 
+        <input type={'submit'} value='test destroy' onClick={()=>{
+          fetch('http://localhost:3000/user/destroy/6330c32214772093ed83dcfb', {
+            method: 'DELETE',
+            headers: { 'Content-Type': 'application/json' },
+          }).then((response) => response.json())
+          .then((data) => {
+            console.log(data);
+          });
+        }} />
+
 <input type={'submit'} value='test getDeck' onClick={()=>{
-          fetch('http://localhost:3000/user/deck/632e415c57ee8e5a0673eb7f', {
+          fetch('http://localhost:3000/user/deck/6330bd17bdf09eb088f824df', {
             method: 'GET',
             headers: { 'Content-Type': 'application/json' },
           }).then((response) => response.json())
@@ -96,7 +87,7 @@ function App() {
         }} />
 
 <input type={'submit'} value='test addCard' onClick={()=>{
-          fetch('http://localhost:3000/user/deck/add/632d0fa1705f594726aa8136', {
+          fetch('http://localhost:3000/user/deck/add/6330bd17bdf09eb088f824df/632d0fa1705f594726aa8136', {
             method: 'PUT',
             body: JSON.stringify({}),
             headers: { 'Content-Type': 'application/json' },
@@ -107,7 +98,7 @@ function App() {
         }} />
 
 <input type={'submit'} value='test remCard' onClick={()=>{
-          fetch('http://localhost:3000/user/deck/rem/632d0fa1705f594726aa8136', {
+          fetch('http://localhost:3000/user/deck/rem/6330bd17bdf09eb088f824df/632d0fa1705f594726aa8136', {
             method: 'PUT',
             body: JSON.stringify({}),
             headers: { 'Content-Type': 'application/json' },
@@ -118,7 +109,7 @@ function App() {
         }} />
 
 <input type={'submit'} value='test toggleCard' onClick={()=>{
-          fetch('http://localhost:3000/user/deck/toggle/632d0fa1705f594726aa8136', {
+          fetch('http://localhost:3000/user/deck/toggle/6330bd17bdf09eb088f824df/632d0fa1705f594726aa8136', {
             method: 'PUT',
             body: JSON.stringify({}),
             headers: { 'Content-Type': 'application/json' },
@@ -146,7 +137,7 @@ function App() {
         type={'submit'}
         value='test GetOneById'
         onClick={() => {
-          fetch('http://localhost:3000/cards/getOneById/632d0fa1705f594726aa8136', {
+          fetch('http://localhost:3000/cards/getOneById/632f6bd3bc9100de884549a5', {
             method: 'GET',
             headers: { 'Content-Type': 'application/json' },
           })
